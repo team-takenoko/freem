5eac484985baa08ab535480dd8b5270e86c829c8
bamboo-noko <plumhearts.yuki@gmail.com>
2019-01-28 23:11:37 +0900

    create html for regist content page

---
+++ resources/views/regist_content.blade.php
@@ -0,0 +1,35 @@
+<!doctype html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+        <title>Freem</title>
+    </head>
+    <body class="d-flex flex-column h-100">
+        <header>
+            <nav class="navbar navbar-expand-lg fixed-top navbar-dark bg-dark">
+                <a class="navbar-brand" href="#">Freem</a>
+                <form class="form-inline col-12">
+                    <div class="form-group mx-auto">
+                        <input type="text" class="form-control" id="search" placeholder="Search">
+                        <button type="submit" class="btn btn-dark">Search</button>
+                    </div>
+                </form>
+            </nav>
+        </header>
+
+        <main class="flex-shrink-0" role="main">
+            <div class="container" style="padding: 60px 15px 0px 15px;">
+                <form class="form-inline col-12">
+                    <div class="form-group mx-auto">
+                        <input type="text" class="form-control col-12" id="title" placeholder="Title">
+                        <input type="text" class="form-control col-12" id="description" placeholder="Description">
+                        <button type="submit" class="btn btn-dark col-12">Registration</button>
+                    </div>
+                </form>
+            </div>
+        </main>
+
+    </body>
+</html>
--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,7 @@
 Route::get('/', function () {
     return view('index');
 });
+
+Route::get('upload', function () {
+    return view('regist_content');
+});
